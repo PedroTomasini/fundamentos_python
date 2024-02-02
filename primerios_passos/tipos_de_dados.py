56f0771899d225fdaf49d63f4830dbfc1f7e4d09
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-02 18:02:23 -0300

    Convesão de tipos

---
+++ primerios_passos/tipos_de_dados.py
@@ -0,0 +1,17 @@
+
+print("Olá, seja bem vindo ao curso de Python!")
+
+print(11 +10)
+print(1.5 + 10 + 1000)
+print(True)
+print(False)
+print("Python")
+
+int(11)
+float(1.5)
+str("Python")
+bool(False and True)
+list(1,2,3,4,5)
+dict(mapa)
+set(coleção)
+bytes(binário)
---
+++ primerios_passos/vaiaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Pedro"
+
+idade = 33
+
+nome, idade = ("Marcus", 34)
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "SC", "MG", "PR"]
+
+print(BRAZILIAN_STATES)
