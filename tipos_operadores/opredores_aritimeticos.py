ca57071c25876fc9fe5304aca79d737b344348d6
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-02 20:15:57 -0300

    Operadores aritiméticos

--- tipos_operadores/opredores_aritimeticos.py
+++ tipos_operadores/opredores_aritimeticos.py
@@ -4,9 +4,9 @@ produto_2 = 10
 print(produto_1 + produto_2)
 print(produto_1 - produto_2)
 print(produto_1 / produto_2)
-print(produto_1 // produto_2)
+print(produto_1 // produto_2) # mostra o resultado da divisão apenas com número inteuro
 print(produto_1 * produto_2)
-print(produto_1 % produto_2)
+print(produto_1 % produto_2) # mostra o velor que sobra 
 print(produto_1 ** produto_2)
 
 x = (10 + 5) * 4
