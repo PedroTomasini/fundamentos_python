3776787160385b2b9076a50f9e1f17f6958cdc24
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-02 23:28:40 -0300

    Operadores de associação

---
+++ tipos_operadores/operadores_associacao.py
@@ -0,0 +1,8 @@
+# São usados para verificar se um objeto está em uma sequencia.
+
+fruta = ["Uva", "Maça"]
+curso = "Curso de Python"
+
+print("Laranja" in fruta)
+print("python" not in curso)
+print("Uva" in fruta)
