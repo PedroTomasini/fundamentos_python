d866b969bff670dd3762f4d384ed0cdfc0c45c3c
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-02 21:04:38 -0300

    Operadores de atribuição

---
+++ tipos_operadores/operadores_atribuicao.py
@@ -0,0 +1,26 @@
+saldo = 500
+print(saldo) # 500
+
+saldo = 200
+print(saldo) # 200
+
+saldo += 10
+print(saldo) # 210
+
+saldo -= 5
+print(saldo) # 205
+
+saldo //= 2
+print(saldo) # 102
+
+saldo /= 2
+print(saldo) # 51.0
+
+saldo *= 10
+print(saldo) # 510.0
+
+saldo %= 4
+print(saldo) # 2.0
+
+saldo *= 2
+print(saldo) # 4.0
