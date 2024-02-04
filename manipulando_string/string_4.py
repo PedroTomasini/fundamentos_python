5d85a61759c15e32bd9c79620286e239319cdc5b
Pedro Tomasini <pedrotomasini1@outlook.com>
2024-02-04 19:25:25 -0300

    String multiplas linhas

---
+++ manipulando_string/string_4.py
@@ -0,0 +1,23 @@
+nome = "Pedro"
+
+mensagem = f"""
+    Olá, meu nome é {nome}!
+Estou aprendendo Python.
+    Essa mensagem tem difeintes recuos.
+"""
+
+print(mensagem)
+
+print(
+    """
+    ==========MENU==========
+
+    1 - Sacar
+    2 - Depositar
+    0 - Sair
+
+    ========================
+
+        "Obrigado por usar nosso sistema!"
+"""
+)
